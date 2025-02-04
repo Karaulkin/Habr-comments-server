1bb2c78b8cc4197e9331902eba1ec2f7f2efa671
kirill <karaulkin772@mail.ru>
2025-02-04 10:58:05 +0300

    add no implement sql query

--- internal/storage/pg/pg.go
+++ internal/storage/pg/pg.go
@@ -2,6 +2,7 @@ package pg
 
 import (
 	"Habr-comments-server/internal/config"
+	"Habr-comments-server/internal/models"
 	"context"
 	"fmt"
 	"github.com/jackc/pgx/v5"
@@ -26,10 +27,7 @@ func New(dbCfg config.DB) (*Storage, error) {
 }
 
 func (s *Storage) Stop(ctx context.Context) error {
-	const op = "storage.pg.Stop"
-
 	err := s.db.Close(ctx)
-
 	if err != nil {
 		return err
 	}
@@ -39,5 +37,43 @@ func (s *Storage) Stop(ctx context.Context) error {
 
 //TODO: Return list of posts @SysteamPost
 // Return post by id and return comments @SysteamPost
+// Return comments in comments
 // Write block comments by id post @SysteamPost
 // Add comment on parent or id comment
+
+// Добавляет пост
+func (s *Storage) SavePost(ctx context.Context) (models.Post, error) {
+	const op = "storage.post.New"
+
+	panic("storage.post.New: implement me")
+}
+
+// Возвращает старший комментарий
+func (s *Storage) ParentComments(ctx context.Context, id int) ([]models.Comment, error) {
+	const op = "storage.post.ParentComments"
+	panic("storage.post.ParentComments: implement me")
+}
+
+// Возвращает младший комментарий
+func (s *Storage) CommentsWithParent(ctx context.Context, id int) (models.Comment, error) {
+	const op = "storage.post.CommentsWithParent"
+	panic("storage.post.CommentsWithParent: implement me")
+}
+
+// Блокировка комментариев на посте
+func (s *Storage) ModeBlockComment(ctx context.Context, id int) (models.Comment, error) {
+	const op = "storage.post.ModeBlockComment"
+	panic("storage.post.ModeBlockComment: implement me")
+}
+
+// Добавляет комментарий
+func (s *Storage) SaveComment(ctx context.Context) (models.Comment, error) {
+	const op = "storage.post.AddComment"
+	panic("storage.post.AddComment: implement me")
+}
+
+// Добавляет вложенный комментарий
+func (s *Storage) SaveJuniorComment(ctx context.Context, id int) error {
+	const op = "storage.post.AddJuniorComment"
+	panic("storage.post.AddComment: implement me")
+}
